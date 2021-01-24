ab59adc9799ca9535111afa7ea2aa528f1d857b6
Alexandr Yakovlev <leffalt@gmail.com>
2021-01-24 16:59:22 +0700

    I'll think about later

---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+
+defined(“DBDRIVER”)or define(‘DBDRIVER’,’mysql’);
+defined(“DBHOST”)or define(‘DBHOST’,’localhost’);
+defined(“DBNAME”)or define(‘DBNAME’,’patterns’);
+defined(“DBUSER”)or define(‘DBUSER’,’root’);
+defined(“DBPASS”)or define(‘DBPASS’,’pass’);
